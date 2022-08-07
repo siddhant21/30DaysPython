d672bb61a2561230155d37119c1d527c607236f1
siddhant21 <46870926+siddhant21@users.noreply.github.com>
2022-08-08 02:05:13 +0530

    Revert "trail"

--- Day3/temp.py
+++
@@ -1,8 +0,0 @@
-for i in range(1,6):
-    print("\n")
-    for_one_time = 0
-    for j in range(4):
-        if for_one_time==0:
-            print(i,end=" ")
-        for_one_time = 1
-        print(i**j,end=" ") 
